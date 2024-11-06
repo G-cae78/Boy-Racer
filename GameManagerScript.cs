2a3fafd66248d243e9485d70858ca7371e93c615
George Anayo-Ezikeoha <123216765+G-cae78@users.noreply.github.com>
2024-11-06 16:35:43 +0000

    Game Menu
    implemented game menu

--- GameManagerScript.cs
+++ GameManagerScript.cs
@@ -1,16 +1,22 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour
 {
-    public static GameManagerScript instance;
+    public static GameManagerScript gameManager;
     public Rigidbody playerCar;
+
+    public GameObject gameMenu;
+    public Button pause;
+    public bool enterMenu;
 
     public Vector3 carDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        enterMenu=false;
+        gameMenu.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,4 +24,18 @@ public class GameManagerScript : MonoBehaviour
     {
         
     }
+    public void pauseControl(){
+        if(enterMenu==true){
+            enterMenu=false;
+            gameMenu.SetActive(false);
+        }
+        else{ 
+        enterMenu=true;
+        menu();
+        }
+    }
+    public void menu(){
+        if(enterMenu)
+        gameMenu.SetActive(true);
+    } 
 }
